584dabb0ef6cdac1a99f270d86f090f015e1a94c
Mansoor Jawed <mn359305@dal.ca>
2023-08-31 17:17:44 +0400

    Winning Conditions and Code Refactoring
    Refactored some previous code to avoid duplication and fix some bugs. Game winning conditions made and implemented

--- gameRules.py
+++ gameRules.py
@@ -26,64 +26,73 @@ def playerMove(pPosition, playerSymbol, ttt):
     col = int(pPosition[1:2])
     ttt[row, col] = playerSymbol
     map(ttt)
-    return ttt
+    winner = checkWinner(ttt)
+    return ttt, winner
 
 #Check if a winning condition has been met for someone ot be declared a winner in the game
 def checkWinner(ttt):
-    win = False
-    if colWin() or rowWin(ttt) or diagWin():
-        win = True
-    return win
+    checker = [rowWin(ttt), colWin(ttt), diagWin(ttt)]
+    return any(checker)
 
-def colWin():
-    return
+def colWin(ttt):
+    winner = False
+    colNumber = 3
+    for i in range(colNumber):
+        column = ttt[:,i]
+        compareValues = column == column[0]
+        if (column[0] != " " and compareValues.all()):
+            winner = True
+            break
 
-# def rowWin(ttt):
-#     rowWin = False
-#     for row in ttt:
-#         for cell in row:
-#             if cel
-#
-#     return
+    return winner
 
-def diagWin():
-    return
+def rowWin(ttt):
+    winner = False
+    for row in ttt:
+        if all(cell == row[0] and cell != " " for cell in row):
+            winner = True
+    return winner
+
+
+def diagWin(ttt):
+    winner = False
+    if ((ttt[0][0] == ttt[1][1] == ttt[2][2] != " ")
+            or (ttt[2][0] == ttt[1][1] == ttt[0][2] != " ")):
+        winner = True
+    return winner
 
 def main():
-    running = True
-    while running:
-        player1 = "X"
-        player2 = "O"
-        moveCount = 0
 
-        #mini map
-        ttt = np.full((3,3), " ", dtype=str)
+    player1 = "X"
+    player2 = "O"
+    moveCount = 0
+    winner = False
 
-        ttt = map(ttt)
+    #mini map
+    ttt = np.full((3,3), " ", dtype=str)
+    ttt = map(ttt)
 
-        # stop loop false and game not running
-        if not running:
-            break;
 
-        #check if the game is running
-        gameRunning = True
-        while gameRunning:
-            #player 1 move
-            playerCount = (moveCount % 2) + 1
-            inputMessage = f"Player {playerCount} turn, Enter position: "
-            pPosition = input(inputMessage)
+    #check if the game is running
+    gameRunning = True
+    while gameRunning:
+        #player 1 move
+        playerCount = (moveCount % 2) + 1
+        inputMessage = f"Player {playerCount} turn, Enter position: "
+        pPosition = input(inputMessage)
 
-            if playerCount == 1:
-                ttt = playerMove(pPosition, player1, ttt)
-            else:
-                ttt = playerMove(pPosition, player2, ttt)
-            moveCount += 1
+        if playerCount == 1:
+            ttt, winner = playerMove(pPosition, player1, ttt)
+        else:
+            ttt, winner = playerMove(pPosition, player2, ttt)
+        moveCount += 1
+        print(winner)
 
-            # Stop game if 9 moveCount has been played
-            if moveCount == 9:
-                print("max moves played. Generating new Game: ")
-                gameRunning = False
-                break
+        # Stop game if 9 moveCount has been played
+        if moveCount == 9:
+            print("max moves played. Generating new Game: ")
+            gameRunning = False
+            break
 
 
 if __name__ == "__main__":
