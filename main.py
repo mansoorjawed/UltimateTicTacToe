bf7d40212eff6ab9845ed8098e136a87dc281489
Mansoor Jawed <mn359305@dal.ca>
2023-09-02 11:11:28 +0400

    Update map to detect clicks and a rough graphic of the map
    The map i snow initlaized so it kind of resembles a ultimate tic tac toe board/map and basic mouse click detectable now

--- main.py
+++ main.py
@@ -1,47 +1,87 @@
+import numpy as np
 import pygame
+import sys
 
 # Initialize pygame
 pygame.init()
 
-# Get screen dimensions
-info = pygame.display.Info()
-screen_width = info.current_w
-screen_height = info.current_h
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Screen dimensions
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+# Board configuration
+GRID_SIZE = 3
+CELL_SIZE = SCREEN_WIDTH / GRID_SIZE
+MINI_CELL_SIZE = CELL_SIZE / GRID_SIZE
+BORDER_WIDTH = 2
+THICK_BORDER_WIDTH = 4
+
+# Create the Icons
+X_ICON = pygame.image.load ('X.png')
+X_ICON = pygame.transform.scale(X_ICON, (MINI_CELL_SIZE, MINI_CELL_SIZE))
+
+O_ICON = pygame.image.load('O.png')
+O_ICON = pygame.transform.scale(O_ICON, (MINI_CELL_SIZE, MINI_CELL_SIZE))
+
+# Create the screen and clock objects
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Ultimate Tic Tac Toe")
+clock = pygame.time.Clock()
+
+def drawBoard():
+    # Draw the mini-grids
+    for x in range(GRID_SIZE):
+        for y in range(GRID_SIZE):
+            pygame.draw.rect(screen, BLACK, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE), THICK_BORDER_WIDTH)
+
+            for mini_x in range(1, GRID_SIZE):
+                pygame.draw.line(screen, BLACK, (x * CELL_SIZE + mini_x * MINI_CELL_SIZE, y * CELL_SIZE), (x * CELL_SIZE + mini_x * MINI_CELL_SIZE, (y + 1) * CELL_SIZE), BORDER_WIDTH)
+
+            for mini_y in range(1, GRID_SIZE):
+                pygame.draw.line(screen, BLACK, (x * CELL_SIZE, y * CELL_SIZE + mini_y * MINI_CELL_SIZE), ((x + 1) * CELL_SIZE, y * CELL_SIZE + mini_y * MINI_CELL_SIZE), BORDER_WIDTH)
+
+    # Draw the main grid
+    for i in range(1, GRID_SIZE):
+        pygame.draw.line(screen, BLACK, (i * CELL_SIZE, 0), (i * CELL_SIZE, SCREEN_HEIGHT), THICK_BORDER_WIDTH)
+        pygame.draw.line(screen, BLACK, (0, i * CELL_SIZE), (SCREEN_WIDTH, i * CELL_SIZE), THICK_BORDER_WIDTH)
 
 
-# Calculate percentage-based dimensions
-def calculate_dimensions(percentage, total_dimension):
-    return int(percentage * total_dimension / 100)
-
-
-# Percentage-based dimensions for game window
-game_window_width_percentage = 80
-game_window_height_percentage = 80
-
-game_window_width = calculate_dimensions(game_window_width_percentage, screen_width)
-game_window_height = calculate_dimensions(game_window_height_percentage, screen_height)
-
-# Create the game window
-screen = pygame.display.set_mode((game_window_width, game_window_height))
-pygame.display.set_caption("Tic Tac Toe")
-
-bar_height = 50
-
-# Main game loop
+# Main loop
 running = True
+icon_positions = []
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Get mouse click position and add to the list of icon positions
+            x, y = event.pos
+            icon_positions.append((x, y))
 
-    # Clear the screen
-    screen.fill((255, 255, 255))  # Fill with black background color
 
-    # Draw the white bar
-    pygame.draw.rect(screen, (0, 0, 0), (0, game_window_height - bar_height, game_window_width, bar_height))
 
-    # Update the display
+    screen.fill(WHITE)
+    drawBoard()
+
+    for pos in icon_positions:
+        screen.blit(X_ICON, pos)
+
     pygame.display.flip()
+    clock.tick(15)
 
-# Quit the game
+    # check clicked boxes
+    # numberedBoxes = np.zeros((9,9))
+
+    # if event.type == pygame.MOUSEBUTTONDOWN:
+    #     mouseX, mouseY = pygame.mouse.get_pos()
+    #     print(mouseX // MINI_CELL_SIZE)
+    #     screen.blit(X_ICON, (mouseX, mouseY))
+    #     pygame.display.flip()
+
 pygame.quit()
+sys.exit()
+
